b1bf4efcdc3ef684f4b79564a2b4b73f04e25c59
MaxenceCalifano <48595795+MaxenceCalifano@users.noreply.github.com>
2023-08-11 13:20:54 +0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -33,7 +33,7 @@ async function getRecipes() {
     cache: "default",
     };
     
-    return await fetch('../Data/recipes.json', myInit)
+    return await fetch('../Data/recipes.json')
         .then(res => res.json())
         .then(res => res.recipes)
         .catch(err => console.log('an error occurs', err))
